9e409dd935e0a4b05c3431452088363e808a967f
Subhadeep Das <subhadeepworks.official@gmail.com>
2025-11-22 00:13:08 +0530

    dom(downloader button)

---
+++ dom/download bar/script.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector('button')
+let h = document.querySelector('h2')
+let inner = document.querySelector('#inner')
+let info = document.querySelector('h3')
+
+let count = 0;
+btn.addEventListener('click', function(){
+    let num = 60+Math.floor(Math.random()*40)
+    btn.style.pointerEvents = 'none'
+    info.innerHTML = `Your download will take ${num/10} seconds`
+    
+    let int = setInterval(() => {
+        console.log(count++);
+        inner.style.width = count+'%'
+        h.innerHTML = count+'%'
+    }, num);
+    
+    setTimeout(() => {
+        clearInterval(int)
+        btn.innerHTML = 'Downloaded'
+        btn.style.backgroundColor = 'green'
+    }, num*100);
+})
