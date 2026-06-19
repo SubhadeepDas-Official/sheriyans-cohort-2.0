8e785bc5b05c8ca09362c14df2e8eab71c35edf6
Subhadeep Das <subhadeepworks.official@gmail.com>
2026-06-19 21:40:42 +0530

    new commit

--- BACKEND/lec-89-database/server.js
+++ BACKEND/lec-89-database/server.js
@@ -3,8 +3,13 @@ const app = require("./src/app")
 const mongoose = require("mongoose")
 
 const connectToDb = () => {
-  mongoose.connect("mongodb+srv://subhadeepworksofficial_db_user:PNdgl7xzBX4VXcUu@cluster0.bcnchsa.mongodb.net/")
+  mongoose.connect("mongodb+srv://subhadeepworksofficial_db_user:PNdgl7xzBX4VXcUu@cluster0.bcnchsa.mongodb.net/demo-database")
+  .then(() => {
+    console.log("Connected to Database");
+  })
 }
+
+connectToDb()
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
