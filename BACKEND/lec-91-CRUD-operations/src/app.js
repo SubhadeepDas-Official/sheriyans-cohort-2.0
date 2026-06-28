b728576464072aa3e8e8f7185ee5a301408f3360
Subhadeep Das <subhadeepworks.official@gmail.com>
2026-06-28 23:48:17 +0530

    new commit

--- BACKEND/lec-91-CRUD-operations/src/app.js
+++ BACKEND/lec-91-CRUD-operations/src/app.js
@@ -18,4 +18,13 @@ app.post('/notes', async (req, res) => {
   })
 });
 
+app.get("/notes", async(req, res) => {
+  const notes = await noteModel.find()
+
+  res.status(200).json({
+    message: "Notes fetched successfully",
+    notes
+  })
+})
+
 module.exports = app;
