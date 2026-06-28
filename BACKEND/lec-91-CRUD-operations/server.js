d26e9ed65e9587188a53ac6af9e9c207a4537d12
Subhadeep Das <subhadeepworks.official@gmail.com>
2026-06-28 21:44:21 +0530

    crud ops

--- BACKEND/lec-91-CRUD-operations/server.js
+++ BACKEND/lec-91-CRUD-operations/server.js
@@ -1,14 +1,9 @@
+require("dotenv").config()
+const connectToDb = require("./src/config/database")
+
 const app = require("./src/app")
 
-const mongoose = require("mongoose")
-
-const connectToDb = () => {
-  mongoose.connect("mongodb+srv://subhadeepworksofficial_db_user:PNdgl7xzBX4VXcUu@cluster0.bcnchsa.mongodb.net/demo-database")
-  .then(() => {
-    console.log("Database is successfully connected");
-    
-  })
-}
+connectToDb()
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- BACKEND/lec-91-CRUD-operations/src/app.js
+++ BACKEND/lec-91-CRUD-operations/src/app.js
@@ -1,5 +1,21 @@
-const express = require("express")
+const express = require('express');
+const noteModel = require('./models/notes.model');
 
-const app = express()
+const app = express();
 
-module.exports = app
+app.use(express.json());
+
+app.post('/notes', async (req, res) => {
+  const { title, description } = req.body
+
+  const note = await noteModel.create({
+    title, description
+  })
+
+  res.status(201).json({
+    message: "Note created successfully",
+    note
+  })
+});
+
+module.exports = app;
