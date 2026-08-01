b17a95fa44c477c59a49550dd8d9981a291a7b0b
Subhadeep Das <subhadeepworks.official@gmail.com>
2026-08-01 12:10:22 +0530

    new commit

--- Fullstack/Instagram clone/Backend/src/controller/post.controller.js
+++ Fullstack/Instagram clone/Backend/src/controller/post.controller.js
@@ -18,15 +18,14 @@ const createPostController = async (req, res) => {
     });
   }
 
-
   let decoded = null;
 
   try {
     decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   } catch (error) {
     return res.status(401).json({
-      message: "Unauthorized access!"
-    })
+      message: 'Unauthorized access!',
+    });
   }
 
   // console.log(decoded);
@@ -34,7 +33,7 @@ const createPostController = async (req, res) => {
   const uploadedFile = await imageKit.files.upload({
     file: await toFile(req.file.buffer, req.file.originalname),
     fileName: `profile_${Date.now()}`,
-    folder: "Cohort-2-Insta-clone-posts" 
+    folder: 'Cohort-2-Insta-clone-posts',
   });
 
   const post = await postModel.create({
@@ -48,6 +47,86 @@ const createPostController = async (req, res) => {
   });
 };
 
+const getPostController = async (req, res) => {
+  const token = req.cookies.user_login_token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized access!',
+    });
+  }
+
+  let decoded = null;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Token Invalid!',
+    });
+  }
+
+  const userId = decoded.id;
+
+  const posts = await postModel.find({
+    user: userId,
+  });
+
+  console.log(posts);
+
+  res.status(200).json({
+    message: 'Post fetched successfully!',
+    post: posts
+  });
+};
+
+const getPostDetailsController = async(req, res) => {
+  const token = req.cookies.user_login_token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized access!',
+    });
+  }
+
+  let decoded = null;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Token Invalid!',
+    });
+  }
+  
+  const userId = decoded.id
+  const postId = req.params.postId
+
+  const post = await postModel.findById(postId)
+
+  if(!post){
+    return res.status(404).json({
+      message: "Post cannot be found!"
+    })
+  }
+
+  const isUserValid = post.user.equals(userId)
+
+  if(!isUserValid){
+    return res.status(403).json({
+      message: 'Access Forbidden'
+    })
+  }
+
+  return res.status(200).json({
+    message: "Post details fetched successfully",
+    post
+  })
+
+}
+
 module.exports = {
   createPostController,
+  getPostController,
+  getPostDetailsController
 };
--- Fullstack/Instagram clone/Backend/src/routes/post.routes.js
+++ Fullstack/Instagram clone/Backend/src/routes/post.routes.js
@@ -2,13 +2,21 @@ const express = require('express');
 const multer = require('multer'); //used to read files on express server
 const postController = require('../controller/post.controller');
 
-const upload = multer({ storage: multer.memoryStorage() }); //temporarily stores file in RAM/temporary storage before sending the file to cloud storage
 const postRouter = express.Router();
+
+const upload = multer({ storage: multer.memoryStorage() }); 
+//temporarily stores file in RAM/temporary storage before sending the file to cloud storage
 
 postRouter.post('/create-post',
   upload.single('profile_img'), 
   //"profile_img" -> it should be the same fieldname that is used to send data from frontend to backend ("profile_img" is used in Postman for sending data in form data)
   postController.createPostController,
 );
+
+//get the total number of posts created by a specific user
+postRouter.get('/get-post', postController.getPostController)
+
+//returns details about a specific post through postId. Also checks whether the post actually exists, whether the post belongs to the user the request came from 
+postRouter.get('/details/:postId', postController.getPostDetailsController)
 
 module.exports = postRouter;
